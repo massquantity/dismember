0e9460596dd3c17b5825a361c595d54623ff4cb7
massquantity <wdmjjxg@163.com>
2021-05-09 02:20:17 +0800

    nn

---
+++ sparkdl/src/main/scala/com/mass/sparkdl/nn/Add.scala
@@ -0,0 +1,46 @@
+package com.mass.sparkdl.nn
+
+import scala.reflect.ClassTag
+
+import com.mass.sparkdl.nn.abstractnn.AbstractModule
+import com.mass.sparkdl.tensor.{Tensor, TensorNumeric}
+import com.mass.sparkdl.utils.Table
+
+// element-wise add
+class Add[T: ClassTag]()(implicit ev: TensorNumeric[T]) extends AbstractModule[Table, Tensor[T], T] {
+
+  override def updateOutput(input: Table): Tensor[T] = {
+    output.resizeAs(input(0)).copy(input(0))
+    var i = 1
+    while (i < input.length) {
+      require(input[Tensor[T]](i).isSameSizeAs(input[Tensor[T]](0)), "all inputs must have same size")
+      output.add(input[Tensor[T]](i))
+      i += 1
+    }
+    output
+  }
+
+  override def updateGradInput(input: Table, gradOutput: Tensor[T]): Table = {
+    var i = 0
+    while (i < input.length) {
+      if (i >= gradInput.length) {
+        gradInput.insert(i, Tensor[T]())
+      }
+      require(input[Tensor[T]](i).isSameSizeAs(gradOutput), "input must have same size as output")
+      gradInput[Tensor[T]](i).resizeAs(gradOutput).copy(gradOutput)
+      i += 1
+    }
+    gradInput
+  }
+
+  override def clearState(): this.type = {
+    super.clearState()
+    this
+  }
+}
+
+object Add {
+  def apply[T: ClassTag]()(implicit ev: TensorNumeric[T]): Add[T] = {
+    new Add[T]()
+  }
+}
---
+++ sparkdl/src/main/scala/com/mass/sparkdl/nn/Module.scala
@@ -0,0 +1,65 @@
+package com.mass.sparkdl.nn
+
+import scala.reflect.ClassTag
+
+import com.mass.sparkdl.tensor.{Tensor, TensorNumeric}
+
+object Module {
+
+  def flatten[@specialized(Float, Double) T: ClassTag](parameters: Array[Tensor[T]])(
+      implicit ev: TensorNumeric[T]): Tensor[T] = {
+    val compactedTensor = isCompact(parameters)
+    if (compactedTensor != null) {
+      return compactedTensor
+    }
+
+    var i = 0
+    var length = 0
+    while (i < parameters.length) {
+      require(parameters(i).isContiguous, "parameters should be contiguous")
+      length += parameters(i).nElement()
+      i += 1
+    }
+
+    val result = Tensor[T](length)
+    val resultStorage = result.storage()
+
+    i = 0
+    var offset = 0
+    // by setting parameters close together in resultStorage, the parameters in model become compact.
+    while (i < parameters.length) {
+      System.arraycopy(parameters(i).storage().array(), parameters(i).storageOffset(),
+        resultStorage.array(), offset, parameters(i).nElement())
+      parameters(i).set(resultStorage, offset, parameters(i).size(), parameters(i).stride())
+      offset += parameters(i).nElement()
+      i += 1
+    }
+
+    result
+  }
+
+  def isCompact[@specialized(Float, Double) T: ClassTag](parameters: Array[Tensor[T]])(
+    implicit ev: TensorNumeric[T]): Tensor[T] = {
+    require(parameters.length > 0,
+      "The length of paramters should >= 0" +
+        "parameter length" +
+        s" ${parameters.length}")
+    var i = 1
+    val storage = parameters(0).storage()
+    var length = parameters(0).nElement()
+    val offset = parameters(0).storageOffset()
+    // make sure parameters is shared and contiguous
+    while (i < parameters.length) {
+      if (!storage.eq(parameters(i).storage())) {
+        return null
+      }
+      if (offset + length != parameters(i).storageOffset()) {
+        return null
+      }
+      length += parameters(i).nElement()
+      i += 1
+    }
+
+    Tensor(storage, offset, Array(length))
+  }
+}
