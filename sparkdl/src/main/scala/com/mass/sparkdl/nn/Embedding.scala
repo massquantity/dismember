247559cfb1af0d0955f7756e4862b02b797224c6
massquantity <wdmjjxg@163.com>
2021-05-25 22:54:57 +0800

    embedding

---
+++ sparkdl/src/main/scala/com/mass/sparkdl/nn/Embedding.scala
@@ -0,0 +1,83 @@
+package com.mass.sparkdl.nn
+
+import scala.reflect.ClassTag
+
+import com.mass.sparkdl.nn.abstractnn.TensorModule
+import com.mass.sparkdl.nn.mixin.LookupTable
+import com.mass.sparkdl.tensor.{Tensor, TensorNumeric}
+
+class Embedding[T: ClassTag](
+    val nIndex: Int,
+    val embedSize: Int,
+    val paddingIdx: Int = -1)(
+    implicit ev: TensorNumeric[T]) extends TensorModule[T] with LookupTable[T] {
+
+  var weight: Tensor[T] = Tensor[T](nIndex, embedSize).randn(0.0, 0.05)
+  var gradWeight: Tensor[T] = Tensor[T](nIndex, embedSize).zero()
+  private var inputBuffer = Tensor[Int]()
+  padWeight(weight, paddingIdx)
+
+  override def updateOutput(input: Tensor[T]): Tensor[T] = {
+    inputBuffer = input.contiguous().asInstanceOf[Tensor[Int]]
+    val batchSize = input.size(0)
+    val numEle = inputBuffer.nElement()
+    output.resize(Array(numEle, embedSize))
+
+    val inputData = inputBuffer.storage().array()
+    val inputOffset = inputBuffer.storageOffset()
+    val outputData = output.storage().array()
+    val outputOffset = output.storageOffset()
+    val weightData = weight.storage().array()
+    val weightOffset = weight.storageOffset()
+
+    embeddingLookup(inputData, inputOffset, outputData, outputOffset,
+      weightData, weightOffset, embedSize, nIndex)
+
+    if (input.dim() == 2) {
+      output = output.view(batchSize, input.size(1), embedSize)
+    }
+
+    output
+  }
+
+  override def updateGradInput(input: Tensor[T], gradOutput: Tensor[T]): Tensor[T] = {
+    if (!gradInput.isSameSizeAs(input)) {
+      gradInput.resizeAs(input).zero()
+    }
+    gradInput
+  }
+
+  override def accGradParameters(input: Tensor[T], gradOutput: Tensor[T]): Unit = {
+    require(gradWeight.isContiguous, "Embedding: gradWeight must be contiguous")
+    inputBuffer = input.contiguous().asInstanceOf[Tensor[Int]]
+    val inputData: Array[Int] = inputBuffer.storage().array()
+    val inputOffset: Int = inputBuffer.storageOffset()
+    val _gradOutput = gradOutput.contiguous()
+    val gradWeightData = gradWeight.storage().array()
+    val gradWeightOffset = gradWeight.storageOffset()
+    val gradOutputData = _gradOutput.storage().array()
+    val gradOutputOffset = _gradOutput.storageOffset()
+
+    updateEmbeddings(inputData, inputOffset, gradOutputData, gradOutputOffset,
+      gradWeightData, gradWeightOffset, embedSize, paddingIdx, scaleW)
+
+  }
+
+  override def parameters(): (Array[Tensor[T]], Array[Tensor[T]]) = {
+    (Array(this.weight), Array(this.gradWeight))
+  }
+
+  override def clearState(): this.type = {
+    super.clearState()
+    inputBuffer.set()
+    this
+  }
+}
+
+object Embedding {
+  def apply[@specialized(Float, Double) T: ClassTag](
+      nIndex: Int, nOutput: Int, paddingIdx: Int = -1)(
+      implicit ev: TensorNumeric[T]): Embedding[T] = {
+    new Embedding[T](nIndex, nOutput, paddingIdx)
+  }
+}
---
+++ sparkdl/src/main/scala/com/mass/sparkdl/nn/EmbeddingShare.scala
@@ -0,0 +1,98 @@
+package com.mass.sparkdl.nn
+
+import scala.reflect.ClassTag
+
+import com.mass.sparkdl.nn.abstractnn.AbstractModule
+import com.mass.sparkdl.nn.mixin.LookupTable
+import com.mass.sparkdl.tensor.{Tensor, TensorNumeric}
+import com.mass.sparkdl.utils.{T, Table}
+
+class EmbeddingShare[T: ClassTag](
+    val nIndex: Int,
+    val embedSize: Int,
+    val paddingIdx: Int = -1)(
+    implicit ev: TensorNumeric[T]) extends AbstractModule[Table, Table, T] with LookupTable[T] {
+
+  var weight: Tensor[T] = Tensor[T](nIndex, embedSize).randn(0.0, 0.05)
+  var gradWeight: Tensor[T] = Tensor[T](nIndex, embedSize).zero()
+  private val inputBuffer = T()
+  padWeight(weight, paddingIdx)
+
+  override def updateOutput(input: Table): Table = {
+    var i = 0
+    while (i < input.length) {
+      inputBuffer(i) = input[Tensor[T]](i).contiguous().asInstanceOf[Tensor[Int]]
+      val batchSize = input[Tensor[T]](i).size(0)
+      val numEle = inputBuffer[Tensor[T]](i).nElement()
+      output(i) = output.getOrElse[Tensor[T]](i, Tensor())
+        .resize(Array(numEle, embedSize))
+
+      val index = inputBuffer[Tensor[Int]](i).storage().array()
+      val indexOffset = inputBuffer[Tensor[Int]](i).storageOffset()
+      val outputData = output[Tensor[T]](i).storage().array()
+      val outputOffset = output[Tensor[T]](i).storageOffset()
+      val weightData = weight.storage().array()
+      val weightOffset = weight.storageOffset()
+
+      embeddingLookup(index, indexOffset, outputData, outputOffset,
+        weightData, weightOffset, embedSize, nIndex)
+
+      if (input[Tensor[T]](i).dim() == 2) {
+        output(i) = output[Tensor[T]](i).view(
+          batchSize, input[Tensor[T]](i).size(1), embedSize)
+      }
+      i += 1
+    }
+    output
+  }
+
+  override def updateGradInput(input: Table, gradOutput: Table): Table = {
+    var i = 0
+    while (i < input.length) {
+      gradInput(i) = gradInput.getOrElse[Tensor[T]](i, Tensor())
+        .resizeAs(input(i)).zero()
+      i += 1
+    }
+    gradInput
+  }
+
+  override def accGradParameters(input: Table, gradOutput: Table): Unit = {
+    require(gradWeight.isContiguous, "EmbeddingShare: gradWeight must be contiguous")
+    var i = 0
+    while (i < input.length) {
+      inputBuffer(i) = input[Tensor[T]](i).contiguous().asInstanceOf[Tensor[Int]]
+      val inputData: Array[Int] = inputBuffer[Tensor[Int]](i).storage().array()
+      val inputOffset: Int = inputBuffer[Tensor[Int]](i).storageOffset()
+      val _gradOutput = gradOutput[Tensor[T]](i).contiguous()
+      val gradWeightData = gradWeight.storage().array()
+      val gradWeightOffset = gradWeight.storageOffset()
+      val gradOutputData = _gradOutput.storage().array()
+      val gradOutputOffset = _gradOutput.storageOffset()
+
+      updateEmbeddings(inputData, inputOffset, gradOutputData, gradOutputOffset,
+        gradWeightData, gradWeightOffset, embedSize, paddingIdx, scaleW)
+
+      println(s"gradWeight: ${gradWeightData.mkString("Array(", ", ", ")")}")
+      println(s"gradOutput: ${gradOutputData.mkString("Array(", ", ", ")")}")
+      i += 1
+    }
+  }
+
+  override def parameters(): (Array[Tensor[T]], Array[Tensor[T]]) = {
+    (Array(this.weight), Array(this.gradWeight))
+  }
+
+  override def clearState(): this.type = {
+    super.clearState()
+    inputBuffer.clear()
+    this
+  }
+}
+
+object EmbeddingShare {
+  def apply[@specialized(Float, Double) T: ClassTag](
+      nIndex: Int, nOutput: Int, paddingIdx: Int = -1)(
+      implicit ev: TensorNumeric[T]): EmbeddingShare[T] = {
+    new EmbeddingShare[T](nIndex, nOutput, paddingIdx)
+  }
+}
