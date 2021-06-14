051c38f6802847995c0b874e8c202585cca26998
massquantity <wdmjjxg@163.com>
2021-06-15 01:30:42 +0800

    DeepFM

--- tdm/src/main/scala/com/mass/tdm/model/DeepFM.scala
+++ tdm/src/main/scala/com/mass/tdm/model/DeepFM.scala
@@ -1,37 +1,44 @@
 package com.mass.tdm.model
 
-import scala.reflect.ClassTag
-
 import com.mass.sparkdl.Module
-import com.mass.sparkdl.nn.{Add, FM, Graph, Input, Linear, Embedding, ReLU, Reshape}
-import com.mass.sparkdl.tensor.TensorNumeric
+import com.mass.sparkdl.nn.{Add, Concat, EmbeddingShare, FM, Graph, Input, Linear, ReLU, Reshape}
 import com.mass.tdm.operator.TDMOp
 
 object DeepFM {
 
-  def buildModel[@specialized(Float, Double) T: ClassTag](featSeqLen: Int,
-      embedSize: Int, paddingIdx: Int)(implicit ev: TensorNumeric[T]): Module[T] = {
+  def buildModel(featSeqLen: Int, embedSize: Int, paddingIdx: Int): Module[Float] = {
 
+    val totalLen = featSeqLen + 1
     val numIndex = (math.pow(2, TDMOp.tree.getMaxLevel) - 1).toInt
-    val input = Input[T]()
-    val embedding = Embedding[T](numIndex, embedSize, paddingIdx = paddingIdx)
-      .inputs(input)
+    val inputItem = Input[Float]()
+    val inputSeq = Input[Float]()
+
+    val embedding = EmbeddingShare[Float](numIndex, embedSize, paddingIdx = paddingIdx)
+      .inputs(inputItem, inputSeq)
+    val itemFlatten = Reshape[Float](Array(embedSize))
+      .inputs(Seq((embedding, 0)))
+    val seqFlatten = Reshape[Float](Array(featSeqLen * embedSize))
+      .inputs(Seq((embedding, 1)))
+
     // FM
-    val fm = FM[T]()
-      .inputs(embedding)
+    val fmConcat = Concat[Float]()
+      .inputs(itemFlatten, seqFlatten)
+    val fmFeature = Reshape[Float](Array(totalLen, embedSize))
+      .inputs(fmConcat)
+    val fm = FM[Float]()
+      .inputs(fmFeature)
     // DNN
-    val embeddingReshape = Reshape[T](Array(featSeqLen * embedSize))
-      .inputs(embedding)
-    val linear = Linear[T](featSeqLen * embedSize, featSeqLen)
-      .inputs(embeddingReshape)
-    val relu = ReLU[T]()
+    val dnnFeature = Concat[Float]()
+      .inputs(itemFlatten, seqFlatten)
+    val linear = Linear[Float](totalLen * embedSize, totalLen)
+      .inputs(dnnFeature)
+    val relu = ReLU[Float]()
       .inputs(linear)
-    val linear2 = Linear[T](featSeqLen, 1)
+    val linear2 = Linear[Float](totalLen, 1)
       .inputs(relu)
-    //  val relu2 = ReLU[Float]().inputs(linear2)
-    val add = Add[T]()
+    val add = Add[Float]()
       .inputs(fm, linear2)
 
-    Graph[T](input, add)
+    Graph[Float](Array(inputItem, inputSeq), Array(add))
   }
 }
