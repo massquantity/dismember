477d70afd05836ec7a486c8cdd1821cc09ad4bf7
massquantity <wdmjjxg@163.com>
2022-07-22 22:11:57 +0800

    [DR] MappingOp

---
+++ deep-retrieval/src/main/scala/com/mass/dr/model/MappingOp.scala
@@ -0,0 +1,93 @@
+package com.mass.dr.model
+
+import java.io.{BufferedInputStream, BufferedOutputStream, DataInputStream, OutputStream}
+import java.nio.ByteBuffer
+
+import scala.collection.mutable
+import scala.util.{Failure, Random, Success, Using}
+
+import com.mass.dr.{Path => DRPath}
+import com.mass.dr.model.MappingOp.pathToItems
+import com.mass.dr.protobuf.item_mapping.{ItemSet, Item => ProtoItem, Path => ProtoPath}
+import com.mass.sparkdl.utils.{FileReader => DistFileReader, FileWriter => DistFileWriter}
+
+case class MappingOp(itemIdMapping: Map[Int, Int], itemPathMapping: Map[Int, Seq[DRPath]]) {
+  val idItemMapping: Map[Int, Int] = itemIdMapping.map(i => i._2 -> i._1)
+
+  val pathItemMapping: Map[DRPath, Seq[Int]] = pathToItems(itemPathMapping)
+}
+
+object MappingOp {
+
+  def pathToItems(itemPathMapping: Map[Int, Seq[DRPath]]): Map[DRPath, Seq[Int]] = {
+    itemPathMapping
+      .flatMap { case (item, paths) => paths.map((_, item)) }
+      .groupBy(_._1)
+      .map(i => i._1 -> i._2.values.toSeq)
+  }
+
+  def initItemPathMapping(
+    numItem: Int,
+    numLayer: Int,
+    numNode: Int,
+    numPathPerItem: Int): Map[Int, IndexedSeq[DRPath]] = {
+    (0 until numItem).map(_ -> {
+      val m = for {
+        _ <- 1 to numPathPerItem
+        _ <- 1 to numLayer
+      } yield Random.nextInt(numNode)
+      m.sliding(numLayer, numLayer).toIndexedSeq
+    }).toMap
+  }
+
+  def writeMapping(
+    outputPath: String,
+    itemIdMapping: Map[Int, Int],
+    itemPathMapping: Map[Int, IndexedSeq[DRPath]]
+  ): Unit = {
+    val allItems = ItemSet(
+      itemIdMapping.map { case (item, id) =>
+        val paths = itemPathMapping(id).map(ProtoPath(_))
+        ProtoItem(item, id, paths)
+      }.toSeq
+    )
+
+    val fileWriter = DistFileWriter(outputPath)
+    val output: OutputStream = fileWriter.create(overwrite = true)
+    Using(new BufferedOutputStream(output)) { writer =>
+      val bf: ByteBuffer = ByteBuffer.allocate(4).putInt(allItems.serializedSize)
+      writer.write(bf.array())
+      allItems.writeTo(writer)
+    } match {
+      case Success(_) =>
+        output.close()
+        fileWriter.close()
+      case Failure(t: Throwable) =>
+        throw t
+    }
+  }
+
+  def loadMapping(path: String): MappingOp = {
+    val idMapping = mutable.Map.empty[Int, Int]
+    val pathMapping = mutable.Map.empty[Int, Seq[DRPath]]
+    val fileReader = DistFileReader(path)
+    val input = fileReader.open()
+    Using(new DataInputStream(new BufferedInputStream(input))) { input =>
+      val size = input.readInt()
+      val buf = new Array[Byte](size)
+      input.readFully(buf)
+      val allItems = ItemSet.parseFrom(buf).items
+      allItems.foreach { i =>
+        idMapping(i.item) = i.id
+        pathMapping(i.id) = i.paths.map(_.index.toIndexedSeq)
+      }
+    } match {
+      case Success(_) =>
+        input.close()
+        fileReader.close()
+      case Failure(t: Throwable) =>
+        throw t
+    }
+    MappingOp(Map.empty ++ idMapping, Map.empty ++ pathMapping)
+  }
+}
