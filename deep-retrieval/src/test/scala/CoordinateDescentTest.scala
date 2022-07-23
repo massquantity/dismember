b7141b3af7e086d6023ec88ad42713d7ed101147
massquantity <wdmjjxg@163.com>
2022-07-24 00:20:40 +0800

    [DR] CoordinateDescentTest

---
+++ deep-retrieval/src/test/scala/CoordinateDescentTest.scala
@@ -0,0 +1,72 @@
+import com.mass.dr.dataset.LocalDataSet
+import com.mass.dr.model.LayerModel
+import com.mass.dr.optim.CoordinateDescent
+import com.mass.sparkdl.utils.Engine
+import org.apache.log4j.{Level, Logger}
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class CoordinateDescentTest extends AnyFlatSpec with Matchers {
+
+  Logger.getLogger("com.mass").setLevel(Level.INFO)
+  Engine.setNodeAndCore(1, 8)
+
+  val numLayer = 3
+  val numNode = 100
+  val numPathPerItem = 2
+  val batchSize = 8192
+  val evalBatchSize = 8192
+  val seqLen = 10
+  val minSeqLen = 2
+  val dataPath = s"${System.getProperty("user.dir")}/data/data.csv"
+  val mappingPath = s"${System.getProperty("user.dir")}/data/dr_mapping.txt"
+  val initialize = true
+  val splitRatio = 0.8
+  val embedSize = 16
+  val beamSize = 20
+  val numIteration = 3
+  val decayFactor = 0.999
+  val penaltyFactor = 3e-6
+  val penaltyPolyOrder = 4
+
+  val dataset = LocalDataSet(
+    numLayer = numLayer,
+    numNode = numNode,
+    numPathPerItem = numPathPerItem,
+    trainBatchSize = batchSize,
+    evalBatchSize = evalBatchSize,
+    seqLen = seqLen,
+    minSeqLen = minSeqLen,
+    dataPath = dataPath,
+    mappingPath = mappingPath,
+    initialize = initialize,
+    splitRatio = splitRatio,
+    delimiter = ","
+  )
+
+  val layerModel = LayerModel(dataset.numItem, numNode, numLayer, seqLen, embedSize)
+  val coo = CoordinateDescent(
+    dataset = dataset,
+    batchSize = batchSize,
+    numIteration = numIteration,
+    numCandidatePath = beamSize,
+    numPathPerItem = numPathPerItem,
+    numLayer = numLayer,
+    numNode = numNode,
+    decayFactor = decayFactor,
+    penaltyFactor = penaltyFactor,
+    penaltyPolyOrder = penaltyPolyOrder
+  )
+
+  "CoordinateDescent batch mode" should "update mapping correctly" in {
+    val itemPathMapping = coo.optimize(layerModel, trainMode = "batch")
+    assert(itemPathMapping.keySet == dataset.idItemMapping.keySet)
+    itemPathMapping.values.forall(_.length == numPathPerItem)
+  }
+
+  "CoordinateDescent streaming mode" should "update mapping correctly" in {
+    val itemPathMapping = coo.optimize(layerModel, trainMode = "streaming")
+    assert(itemPathMapping.keySet == dataset.idItemMapping.keySet)
+    itemPathMapping.values.forall(_.length == numPathPerItem)
+  }
+}
--- deep-retrieval/src/test/scala/DeepRetrievalTest.scala
+++ deep-retrieval/src/test/scala/DeepRetrievalTest.scala
@@ -2,7 +2,7 @@ import scala.reflect.runtime.universe.{typeTag, TypeTag}
 
 import com.mass.dr.dataset.LocalDataSet
 import com.mass.dr.loss.CrossEntropyLayer
-import com.mass.dr.model.{DeepRetrieval, LayerModel, RerankModel}
+import com.mass.dr.model.{DeepRetrieval, LayerModel, MappingOp, RerankModel}
 import com.mass.dr.optim.LocalOptimizer
 import com.mass.sparkdl.nn.SampledSoftmaxLoss
 import com.mass.sparkdl.utils.Engine
@@ -22,17 +22,20 @@ class DeepRetrievalTest extends AnyFlatSpec with Matchers {
   val seqLen = 10
   val minSeqLen = 2
   val dataPath = s"${System.getProperty("user.dir")}/data/data.csv"
+  val mappingPath = s"${System.getProperty("user.dir")}/data/dr_mapping.txt"
+  val initialize = true
   val splitRatio = 0.8
   val paddingIdx = -1
   val embedSize = 16
-  val numIteration = 100
+  val numEpoch = 2
+  val reRankEpoch = Some(1)
   val learningRate = 7e-3
   val numSampled = 1
   val topk = 10
   val beamSize = 20
   val progressInterval = 50
 
-  val dataset = new LocalDataSet(
+  val dataset = LocalDataSet(
     numLayer = numLayer,
     numNode = numNode,
     numPathPerItem = numPathPerItem,
@@ -41,55 +44,63 @@ class DeepRetrievalTest extends AnyFlatSpec with Matchers {
     seqLen = seqLen,
     minSeqLen = minSeqLen,
     dataPath = dataPath,
-    mappingPath = None,
-    splitRatio = splitRatio
+    mappingPath = mappingPath,
+    initialize = true,
+    splitRatio = splitRatio,
+    delimiter = ","
   )
-  val drModel = DeepRetrieval(
-    numItem = dataset.numItem,
-    numNode = numNode,
-    numLayer = numLayer,
-    seqLen = seqLen,
-    embedSize = embedSize
-  )
-  drModel.setMapping(dataset)
 
-  val optimizer = new LocalOptimizer(
+  val layerModel = LayerModel(dataset.numItem, numNode, numLayer, seqLen, embedSize)
+  val reRankModel = RerankModel(dataset.numItem, seqLen, embedSize)
+  val optimizer = LocalOptimizer(
     dataset = dataset,
-    drModel = drModel,
-    numIteration = numIteration,
+    layerModel = layerModel,
+    reRankModel = reRankModel,
+    numEpoch = numEpoch,
     numLayer = numLayer,
     learningRate = learningRate,
     numSampled = numSampled,
     embedSize = embedSize,
     topk = topk,
     beamSize = beamSize,
-    progressInterval = progressInterval
+    progressInterval = progressInterval,
+    reRankEpoch = reRankEpoch
   )
-  optimizer.optimize()
+  val optimResults = optimizer.optimize()
+
+  val drModel = DeepRetrieval(
+    layerModel = layerModel,
+    reRankModel = reRankModel,
+    numItem = dataset.numItem,
+    numNode = numNode,
+    numLayer = numLayer,
+    seqLen = seqLen,
+    embedSize = embedSize
+  )
+  val mappingOp = MappingOp(dataset.itemIdMapping, dataset.itemPathMapping)
 
   "DeepRetrieval model" should "initiate with correct classes" in {
     assert(getType(drModel) == "com.mass.dr.model.DeepRetrieval")
-    drModel.layerModel shouldBe a [LayerModel]
-    drModel.reRankModel shouldBe a [RerankModel]
+    layerModel shouldBe a [LayerModel]
+    reRankModel shouldBe a [RerankModel]
     optimizer.layerCriterion shouldBe a [CrossEntropyLayer]
     optimizer.reRankCriterion shouldBe a [SampledSoftmaxLoss[_]]
   }
 
   "Loss in DeepRetrieval model" should "decrease during optimization" in {
-    val evalResults = optimizer.optimEvalResults.map(_.meanMetrics)
+    val evalResults = optimResults.map(_.meanMetrics)
     evalResults reduce { (former, later) =>
-      former.layerLoss lazyZip later.layerLoss foreach((a, b) => assert(a > b))
-      assert(former.reRankLoss > later.reRankLoss)
+      former.layerLoss lazyZip later.layerLoss foreach((a, b) => assert(a >= b))
+      assert(former.reRankLoss >= later.reRankLoss)
       later
     }
   }
 
   "DeepRetrieval model" should "return correct recommendations" in {
     val sequence = Array(1, 2, 3, 4, 5, 6, 7, 89, 2628, 1681)
-    val recommendResult = drModel.recommend(sequence, 10, 20).map(_._1)
+    val recommendResult = drModel.recommend(sequence, 10, 20, mappingOp).map(_._1)
     recommendResult.length should be >= 5
-    // recommendResult should contain atLeastOneOf (2628, 2997, 858)
-    val totalTime = (1 to 100).map(_ => time(drModel.recommend(sequence, 10, 20)))
+    val totalTime = (1 to 100).map(_ => time(drModel.recommend(sequence, 10, 20, mappingOp)))
     println(f"average recommend time: ${totalTime.sum / totalTime.length}%.4fms")
   }
 
