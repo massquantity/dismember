2df63f264d8850f4316973ccaaec34091144fd4a
massquantity <wdmjjxg@163.com>
2023-02-01 18:30:01 +0800

    [test] test JTM

--- jtm/src/test/scala/JtmAsyncSpec.scala
+++ jtm/src/test/scala/JtmAsyncSpec.scala
@@ -1,32 +1,40 @@
+import java.io.File
+
 import com.mass.jtm.optim.JTMAsync
 import com.mass.jtm.tree.TreeUtil
 import com.mass.scalann.utils.Engine
 import com.mass.scalann.utils.Property.filePath
+import org.apache.commons.io.FileUtils
 import org.apache.log4j.{Level, Logger}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.Inspectors.forAll
 
-class JTMAsyncTest extends AnyFlatSpec with Matchers {
-  Logger.getLogger("com.mass").setLevel(Level.INFO)
+class JtmAsyncSpec extends AnyFlatSpec with Matchers {
 
-  val numThread = 8
-  Engine.setCoreNumber(numThread)
-  val prefix = s"${filePath("jtm")}/data/"
+  Logger.getLogger("com.mass").setLevel(Level.INFO)
+  Engine.setCoreNumber(8)
+
+  val dataPath = s"${filePath("jtm")}/data/jtm/train_data.csv"
+  val treePath = s"${filePath("jtm")}/data/jtm/example_tree.bin"
+  val modelPath = s"${filePath("jtm")}/data/jtm/example_model.bin"
+  val testPath = s"${filePath("jtm")}/test_path"
+  FileUtils.forceMkdir(new File(testPath))
+
   val jtm = JTMAsync(
-    dataPath = prefix + "train_data.csv",
-    treePath = prefix + "example_tree.bin",
-    modelPath = prefix + "example_model.bin",
+    dataPath = dataPath,
+    treePath = treePath,
+    modelPath = modelPath,
     gap = 2,
     seqLen = 10,
     hierarchical = false,
     minLevel = 0,
-    numThreads = numThread,
+    numThreads = 8,
     useMask = true
   )
   val projection = jtm.optimize()
   val treeMeta = TreeUtil.getTreeMeta(jtm)
-  TreeUtil.writeTree(jtm, projection, prefix + "jtm_tree.bin")
+  TreeUtil.writeTree(jtm, projection, s"$testPath/jtm_tree.bin")
 
   "Final projection" should "have correct leaf size" in {
     projection should have size treeMeta.leafNum
@@ -43,4 +51,6 @@ class JTMAsyncTest extends AnyFlatSpec with Matchers {
     projection.values.min should be >= minLeafCode
     projection.values.max should be <= maxLeafCode
   }
+
+  FileUtils.deleteDirectory(FileUtils.getFile(testPath))
 }
